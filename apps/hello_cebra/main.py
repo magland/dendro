1588d7f55e8aa6458f4a9b3d9da8f1ece1886afe
Jeremy Magland <jmagland@flatironinstitute.org>
2024-07-01 11:12:21 -0400

    Deleted CEBRA embedding processors 1-4

--- apps/hello_cebra/main.py
+++ apps/hello_cebra/main.py
@@ -1,8 +1,4 @@
 from pairio.sdk import App
-from CebraNwbEmbedding1 import CebraNwbEmbedding1
-from CebraNwbEmbedding2 import CebraNwbEmbedding2
-from CebraNwbEmbedding3 import CebraNwbEmbedding3
-from CebraNwbEmbedding4 import CebraNwbEmbedding4
 from CebraNwbEmbedding5 import CebraNwbEmbedding5
 
 app = App(
@@ -10,10 +6,6 @@ app = App(
     description='Example CEBRA processors'
 )
 
-app.add_processor(CebraNwbEmbedding1)
-app.add_processor(CebraNwbEmbedding2)
-app.add_processor(CebraNwbEmbedding3)
-app.add_processor(CebraNwbEmbedding4)
 app.add_processor(CebraNwbEmbedding5)
 
 if __name__ == '__main__':
