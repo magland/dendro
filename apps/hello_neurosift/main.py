b03a121e1a5df8fbd06675bc3fa3047601513162
Jeremy Magland <jmagland@flatironinstitute.org>
2024-07-12 10:22:50 -0400

    Add AviToMp4Processor to hello_neurosift app

--- apps/hello_neurosift/main.py
+++ apps/hello_neurosift/main.py
@@ -1,6 +1,7 @@
 from pairio.sdk import App
 from UnitsSummary1 import UnitsSummary1
 from EphysSummary1 import EphysSummary1
+from AviToMp4 import AviToMp4Processor
 
 app = App(
     app_name='hello_neurosift',
@@ -9,6 +10,7 @@ app = App(
 
 app.add_processor(UnitsSummary1)
 app.add_processor(EphysSummary1)
+app.add_processor(AviToMp4Processor)
 
 if __name__ == '__main__':
     app.run()
