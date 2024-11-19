318366fecd3c47ca9e8845ca0c458ee5738c46da
Jeremy Magland <jmagland@flatironinstitute.org>
2024-11-19 12:25:35 -0500

    always use call_python=True

--- python/dendro/sdk/_make_spec_file.py
+++ python/dendro/sdk/_make_spec_file.py
@@ -13,17 +13,20 @@ def make_app_spec_file_function(app_dir: str, spec_output_file: Union[str, None]
 
     script_path = str(app_dir_path / 'main.py')
 
-    # check whether script path is executable or has a .py extension
-    call_python = False
-    if not os.access(script_path, os.X_OK):
-        if script_path.endswith('.py'):
-            call_python = True
-        else:
-            raise Exception(f"Script {script_path} is not executable and does not have a .py extension")
+    call_python = True
+
+    # At some point we may want to support non-python executables - so we'd use something like the following
+    # call_python = False
+    # if not os.access(script_path, os.X_OK):
+    #     if script_path.endswith('.py'):
+    #         call_python = True
+    #     else:
+    #         raise Exception(f"Script {script_path} is not executable and does not have a .py extension")
 
     env = os.environ.copy()
     env['SPEC_OUTPUT_FILE'] = spec_output_file
     cmd = ['python', script_path] if call_python else [script_path]
+    print(cmd)
     subprocess.run(cmd, env=env)
 
     # When we do it the following way, the inspection of type hints in the processor context does not work
