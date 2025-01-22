a9f459a8ab5998c03d90ef67d540f73abfb6362b
luiz <luiz_tauffer@hotmail.com>
2025-01-22 16:40:37 +0100

    remove pydantic imports from init

--- python/src/dendro/__init__.py
+++ python/src/dendro/__init__.py
@@ -1,8 +1,5 @@
 import os
-from pydantic import VERSION as pydantic_version # noqa
 
-# In the future we may want to intercept and override the pydantic BaseModel
-from pydantic import BaseModel, Field # noqa
 
 # read the version from thisdir/version.txt
 thisdir = os.path.dirname(os.path.realpath(__file__))
